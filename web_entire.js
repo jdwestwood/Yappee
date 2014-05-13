5c83b9ef8099e367fc0c9a1828e5cff93aac74ff
John Westwood <johnjai@sbcglobal.net>
2014-05-13 13:38:51 -0700

    Added CORS to EPO query handling to work around EPO OPS API blacklisting of some Heroku servers; run EPO queries from dev server using cross-domain ajax with CORS.

--- web_entire.js
+++ web_entire.js
@@ -1,12 +1,14 @@
-// this version of web.js serves only the main page and the Google content; it relies on web_EPO.js on port 8081 to serve
-// the EPO biblio data; the servers are separated in this way because the EPO OPS API blacklists some of the Heroku
-// servers; get around this by always querying EPO from my AWS development server, even when the app is deployed on
-// Heroku.
+// 5/13/2014: this is the original version of web.js that combines serving the main page, the Google content, and the EPO
+// query results; when I deployed to Heroku, I discovered that EPO OPS blacklists some of the Heroku servers; so I
+// moved EPO querying out of web.js into web_EPO.js; I now deploy web.js on Heroku and run web_EPO.js on my AWS
+// development server.
 
 var express = require('express');                         // use 'npm install express' to install this nodejs package
 var http = require('http');                               // part of nodejs
 var https = require('https');
 var fs = require('fs');
+var request = require('request');                         // external package for http requests
+var cheerio = require('cheerio');   // external package like jQuery for manipulating HTML and XML docs (does not execute scripts)
 var crypto = require('crypto');                           // part of nodejs
 
 // memcached (local server) or MemCachier (Heroku) client; on local server; start memcached server using 'memcached -vv -d';
@@ -18,6 +20,13 @@ var MemJS = require('memjs').Client;      // external package; memjs is the memc
 // to 127.0.0.1:11211.
 var memjs = MemJS.create("", {expires: ONE_WEEK});
 
+// var buf = require('buf');
+
+// EPO access:
+const CONSUMER_KEY = process.env.CONSUMER_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+var GETTING_ACCESS_TOKEN = false;               // do not persist an access key; it just causes problems!
+var QUERYING_FOR_BIBLIO_DATA = false;
 // secret for signed cookies; use an environmental variable so secret stays the same across server restarts
 const COOKIE_SECRET = process.env.COOKIE_SECRET;
 
@@ -63,6 +72,7 @@ var sessionList = {};
 app.use(express.logger('default'));
 // give access to 'lib' directory tree so can serve .css and .js files referenced in index.html  
 app.use(express.static(__dirname + '/lib'));
+app.use('/epoapi/biblio/', express.bodyParser()); // bodyParser is used by express request.body to parse the body of a POST request
 
 var indexHTML = fs.readFileSync(indexFile, {'encoding': 'utf-8'});           // returns a buffer unless encoding is specified
 
@@ -151,11 +161,89 @@ app.post('/*', function(clientReq, serverResp) {     // clientReq is an instance
     clientReqLogging(clientReq, 'POST');
     switch (true) {
       case url == '/' || url == '/yappee/':            // do not respond to POST request
-        denyPOSTReq('In processPOSTReq, received POST request for main page');
         break;
-      case url == '/epoapi/biblio':
-        // response to EPO biblio requests is handled in a separate server web_EPO.js listening on port 8081
-        denyPOSTReq('In processPOSTReq, unexpectedly received POST request for EPO biblio data on port 8080');
+      case url == '/epoapi/biblio/':
+        express.bodyParser(clientReq);                   // make body of POST request available via clientReq.body
+        var cacheKey = clientReq.body['CacheKey'];
+        debug_log("\nCacheKey from clientReq: " + cacheKey);
+        var nTries = 0;                                  // number of attempts to retrieve the EPO biblio data
+        if (cacheKey) {                                  // requesting the results to be pulled from cache; if not
+          memjs.get(cacheKey, on_cacheget_EPO_data);
+        }
+        else {
+          getNewBiblioData();
+        }
+
+        function on_cacheget_EPO_data(err, EPO_biblio_buf, key) {
+          // callback for memjs.get for GET POST request for EPO data; EPO_biblio_buf and key are buffers if the key is
+          // and null otherwise
+          if (EPO_biblio_buf) {
+            debug_log("Sending biblio data for " + cacheKey + " from cache");
+            serverResp.send(EPO_biblio_buf.toString());
+            return;
+          }
+          else {
+            debug_log("Key" + cacheKey + " not in cache. Query EPO...");
+            getNewBiblioData();
+          }
+        }
+
+        function getNewBiblioData() {
+          // request an access token from EPO and call back getEPOBiblio when done;
+          getAccessToken(getEPOBiblio);                                     // getEPOBiblio is the callback
+        }
+
+        function getEPOBiblio(access_token, error_message) {              // callback function for getAccessToken
+          QUERYING_FOR_BIBLIO_DATA = true;
+          setTimeout(function() {QUERYING_FOR_BIBLIO_DATA = false;}, 100);    // space EPO API queries at least 100 msec apart
+          if (access_token) {
+            patent_list = clientReq.body['Request Body'];
+            getEPOBiblioData(access_token, patent_list, sendEPOData);     // sendEPOData is the callback
+            nTries += 1;
+          }
+          else {
+           sendEPOData(null, error_message);                              // send error_message from getAccessToken
+          }
+        }
+
+        function sendEPOData(jsonStr, error_message) {                    // callback function for getEPOBiblioData
+          if (jsonStr) {                                                  // got EPO biblio data so send it and cache it
+            debug_log("In sendEPODdata, cacheKey is: " + cacheKey);
+            serverResp.send(jsonStr);
+            if (cacheKey) {
+              memjs.set(cacheKey, jsonStr, on_cacheset_EPO_data, ONE_WEEK);
+            }            
+          }
+          else {                                                          // did not get EPO biblio data back
+            debug_log("In sendEPOData, nTries: ", nTries);
+            if (error_message.message == "invalid_access_token" && nTries == 1) {   // either an invalid access token ...
+              nTries += 1;
+              debug_log("In sendEPOData, access token was invalid (probably expired); get a fresh one");
+              memjs.delete("access_obj", on_delete_expired_EPO_access_token);
+            }
+            else {                                                                  // or some other error
+             debug_log("In sendEPOData, error in getting access token or querying EPO API: " + JSON.stringify(error_message));
+             serverResp.send(JSON.stringify(error_message));
+            }
+          }
+        }
+
+        function on_delete_expired_EPO_access_token(err, success) {
+        // access token was expired at EPO but not in memcache; callback for memjs.delete in sendEPOData
+          // get a valid access token (this time from EPO).
+          getAccessToken(getEPOBiblio);                               // getEPOBiblio is the callback        
+        }
+
+        function on_cacheset_EPO_data(err, success) {
+        // callback for memjs.set in sendEPOData; 'success' is true if the .set succeeded
+          if (success) {
+            debug_log("Cached EPO biblio data with key " + cacheKey);
+          }
+          else {
+            debug_log("In on_cache_EPO_data, cache failed");
+          }
+        }
+
         break;
       default:
         googleReqHeader = prepGoogleReqHeader(clientReq);
@@ -176,6 +264,185 @@ app.post('/*', function(clientReq, serverResp) {     // clientReq is an instance
     console.log("\nResponded to POST request with 404 error");
   }
 });
+
+function looksLikeJSON(jsonStr) {
+  return (jsonStr[0] == '{' && jsonStr.slice(-1) == '}');
+}
+
+function getEPOBiblioData(access_token, patent_list, callback) {
+  // get the requested EPO bibliographic patent data using the access_token;
+  // the callback function is sendEPOData with arguments jsonStr containing the data
+  // in json format and error_message (JSON object)
+  console.log("Querying EPO API for patent biblio data at " + Date.now()); 
+  request.post({url: "https://ops.epo.org/3.1/rest-services/published-data/publication/epodoc/biblio",
+                headers: {"Authorization": "Bearer " + access_token,
+                          "Accept": "application/json"},
+                // also sets header Content-Type: "application/x-www-form-urlencoded; charset=UTF-8")
+                form: {"Request Body": patent_list}
+                },
+      function(error, response, body) {               // error is a request error object, not an HTTP error
+        console.log("\nIn getEPOBiblioData, response statusCode: " + response.statusCode);
+        console.log("In getEPOBiblioData, EPO usage response headers are:" +
+                    "\n  date: " + response.headers['date'] +
+                    "\n  x-individualquotaperhour-used: " + response.headers['x-individualquotaperhour-used'] +
+                    "\n  x-registeredquotaperweek-used: " + response.headers['x-registeredquotaperweek-used'] +
+                    "\n  x-throttling-control: " + response.headers['x-throttling-control'] +
+                    "\n  x-rejection-reasonl: " + response.headers['x-rejection-reason']);
+
+        if (!error) {
+          switch (response.statusCode) {
+            case 200:
+              if (looksLikeJSON(body)) {              // if it looks like JSON, call back with patent data
+                debug_log("In getEPOBiblioData, sending patent data");
+                callback(body, null);
+              }
+              else {                                  // it is an error message in XML - should never happen with statusCode 200???
+                var error_message = getEPOError(response, body);
+                console.log("In getEPOBiblioData, error from EPO server: " + JSON.stringify(error_message));
+                callback(null, error_message);
+              }
+              break;
+            case 400: case 401: case 403: case 404: case 405: case 413: case 500: case 503: case 504: default:
+              var error_message = getEPOError(response, body);
+              console.log("In getEPOBiblioData, error from EPO server: " + JSON.stringify(error_message));
+              callback(null, error_message);
+              break;
+          }
+        }
+        else {
+          var error_message = getHTTPError(response, "", body, "", "Error from request in getEPOBiblioData");
+          console.log("In getEPOBiblioData, HTTP error: " + JSON.stringify(error_message));
+          console.log("Error object from request: ", error);
+          callback(null, error_message);
+        }
+     });
+}
+
+function getAccessToken(callback) {
+  // get the EPO access token; check cache, and if not there get one from EPO
+  // the callback function arguments are access_token (string), error_message (JSON object)
+  if (!GETTING_ACCESS_TOKEN && !QUERYING_FOR_BIBLIO_DATA) {
+    GETTING_ACCESS_TOKEN = true;
+    memjs.get('JSON_access', on_get_access_token); 
+  }
+  else {
+    debug_log("Waiting for previous query for access token or patent data to complete...");
+    setTimeout(getAccessToken, 15, callback);
+  }
+
+  function on_get_access_token(err, access_buf, key) {
+  // callback for memjs.get access_obj above; access_buf is a buffer containing access_obj
+    if (access_buf) {
+      var JSON_access = access_buf.toString();
+      var access_obj = JSON.parse(JSON_access);
+      debug_log("In getAccessToken, got EPO access token from cache: " + access_obj['access_token']);
+      GETTING_ACCESS_TOKEN = false;
+      callback(access_obj['access_token'], null);
+    }
+    else {
+      debug_log("In getAccessToken, failed to get access key from cache, so query EPO for one.");
+      getNewAccessToken(callback);
+    }
+  }
+}
+
+function getNewAccessToken(callback) {
+  // POST request to EPO for new access token;
+  // call the callback with arguments access_token and error_message when done.
+  request.post({url: "https://ops.epo.org/3.1/auth/accesstoken",
+                auth: {"user": CONSUMER_KEY,
+                       "pass": SECRET_KEY},
+                // also sets header Content-Type: "application/x-www-form-urlencoded; charset=UTF-8")
+                form: {"grant_type": "client_credentials"}
+                },
+     function(error, response, JSON_access) {             // error is a request error object, not an HTTP error
+       debug_log("In getNewAccessToken response statusCode: " + response.statusCode + "; with JSON: " + JSON_access);
+       if (!error) {
+         switch (response.statusCode) {
+           case 200:
+             if (looksLikeJSON(JSON_access)) {            // if it looks like JSON, cache it and call back
+               // 'access_token' and 'expires_in' for the token and expiration time, which is a string in sec
+               var access_obj = JSON.parse(JSON_access);
+               var access_token = access_obj["access_token"];
+               var expires = parseInt(access_obj["expires_in"]);
+               memjs.set('JSON_access', JSON_access, on_set_access_token, expires);
+               GETTING_ACCESS_TOKEN = false;
+               debug_log("New access token from EPO: " + access_token);
+               callback(access_token, null);
+             }
+             else {                                     // it is an error message in XML
+               var error_message = getEPOError(response, body);
+               console.log("In getNewAccessToken, error from EPO server: ", JSON.stringify(error_message));
+               callback(null, error_message);
+             }
+             break;
+           case 400: case 401: case 403: case 404: case 405: case 413: case 500: case 503: case 504: default:
+             var error_message = getEPOError(response, body);
+             console.log("In getNewAccessToken, error from EPO server: " + JSON.stringify(error_message));
+             callback(null, error_message);
+             break;
+         }
+       }
+       else {
+         var error_message = getHTTPError(response, "", body, "", "Error from request in getNewAccessToken");
+         console.log("In getNewAccessToken, HTTP error: " + JSON.stringify(error_message));
+         console.log("Error object from request: ", error);
+         callback(null, error_message);
+       }
+
+       function on_set_access_token(err, success) {
+       // callback for memjs.set for JSON_access
+         if (success) {
+           debug_log("In getNewAccessToken, set new EPO access JSON in cache.");
+         }
+         else {
+           debug_log("In getNewAccessToken, failed to set new EPO access JSON in cache");
+         }
+       }
+     });
+}
+
+function getEPOError(response, body) {
+  // parse an EPO error response body, which is in XML
+  var $ = cheerio.load(body, {xmlMode: true});
+  var EPO_error_code = $("code").text();
+  var EPO_error_message = $("message").text();
+  var EPO_error_description = $("description").text();
+   // XML starts with <error> tag
+   // 400 message could be invalid_request, invalid_client, unsupported_grant_type, invalid_access_token
+   // 401 (Unauthorized) status code to indicate which HTTP authentication schemes are supported
+   // 403 description could be "This request has been rejected due to the violation of Fair Use policy" (usage rate exceeded)
+   //     (no message)         "This request has been rejected" if resource is blacklisted (due to too busy or other cause,
+   //                           see my EPO notes)
+   //                          "Developer account is blocked" (uh-oh).
+  if ($("error").length > 0) {                             // an <error> tag encloses the error message
+    var error_message = getHTTPError(response, EPO_error_code, EPO_error_message, EPO_error_description,
+                                     "EPO request layer 2 error");
+  }
+   // XML starts with <fault> tag
+   // 400 code could be CLIENT.InvalidQuery or CLIENT.CQL
+   // 403 code CLIENT.RobotDetected
+   // 404 code could be CLIENT.InvalidReference, CLIENT.WrongReferenceFormatting, CLIENT.NotFound, SERVER.EntityNotFound
+   // 405 code CLIENT.MethodNotAllowed
+   // 413 code CLIENT.Ambiguous Request
+   // 500 code SERVER.DomainAccess (request could not be processed; try again later)
+   // 503 code SERVER.LimitedServerResources (Temporarily unavailable)
+   // 504 code SERVER.????                   (Please reduce query size)
+  else {                                                   // a <fault> tag encloses the error message
+    var error_message = getHTTPError(response, EPO_error_code, EPO_error_message, EPO_error_description,
+                                     "EPO request layer 1 error");
+  }
+  return error_message;
+}
+
+function getHTTPError(response, code, message, description, source) {
+  // create an error object using response.statusCode and the message string
+  return {"Response status code": response.statusCode,
+          "EPO error code": code,
+          "message": message,
+          "description": description,
+          "source": source};
+}
 
 function prepGoogleReqHeader(clientReq) {
   console.log('\nIn prepGoogleReqHeader, received clientReq from ' + clientReq.ip + ' for host \'' + clientReq.headers['host']
